bc808344ebb8d02303230a43190afaae59f8479a
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-03 12:25:35 -0500

    adds ability to decrement

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ class App extends Component {
         <div key={score.id}>
           <h2>Name: {score.name} // Score: {score.score}</h2>
           <button onClick={() => this._incrementScoreById(score.id)}>+</button>
+          <button onClick={() => this._decrementScoreById(score.id)}>-</button>
         </div>
       );
     });
@@ -104,7 +105,19 @@ _incrementScoreById(id) {
     return scoreObj.id !== id ? scoreObj: {...scoreObj, score: scoreObj.score + 1}
   });
   // and call this.setState
-    // and call this.setState
+  this.setState({
+    scores: newScores
+  });
+
+}
+
+_decrementScoreById(id) {
+  //find the player in this.state.scores
+  // decrement their score
+  const newScores = this.state.scores.map(scoreObj => {
+    return scoreObj.id !== id ? scoreObj: {...scoreObj, score: scoreObj.score - 1}
+  });
+  // and call this.setState
   this.setState({
     scores: newScores
   });
