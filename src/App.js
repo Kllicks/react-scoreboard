ce9b54ef4cc8ab4ca694bbb1ac443645220d0184
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-03 11:04:46 -0500

    finishes first version of incrementScoreById

--- src/App.js
+++ src/App.js
@@ -56,10 +56,22 @@ class App extends Component {
   _incrementScoreById(id) {
     // find the player in this.state.scores
     // increment their score
+    const newScores = this.state.scores.map(score => {
+      if (score.id !== id) {
+        return score;
+      } else {
+        return {
+          id: score.id,
+          name: score.name,
+          score: score.score + 1
+        }
+      }
+    });
+    
     // and call this.setState
-  
-    console.log(`test`);
-  
+    this.setState({
+      scores: newScores
+    });
   }
 }
 
